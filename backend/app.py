4f2e37195d7976870756e993f4d01344496032fc
AbhinavGadde <abhinavgadde780@gmail.com>
2025-12-15 16:13:08 +0530

    backend2

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = FastAPI(title="E-Waste Management & Recycling Portal")
 app.add_middleware(
     CORSMiddleware,
     allow_origins = [
-        "https://e-waste-management-sooty.vercel.app"
+        "https://e-waste-management-sooty.vercel.app",
         "http://localhost:5173",
     ],
     allow_credentials=True,
