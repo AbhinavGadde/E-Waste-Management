f365bcb2204c83841cefdd0be4c7a3e002022f5b
AbhinavGadde <abhinavgadde780@gmail.com>
2025-12-15 15:42:11 +0530

    backend

--- backend/app.py
+++ backend/app.py
@@ -17,15 +17,13 @@ from routers import auth, users, recyclers, admin, reports, ml, analytics
 app = FastAPI(title="E-Waste Management & Recycling Portal")
 
 # CORS: allow local dev frontends
-origins = [
-    "https://e-waste-management-sooty.vercel.app/"
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins = [
+        "https://e-waste-management-sooty.vercel.app"
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
